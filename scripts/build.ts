055cbf33f5e03c5c3637a54edd97bbb29ee7b007
K. Akatsuki <thevortexspirit@gmail.com>
2021-02-23 19:20:10 -0500

    refactor: improve build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,30 +1,23 @@
 import { readFileSync, writeFileSync } from 'fs';
-import { resolve } from 'path';
-import { format } from 'prettier';
+import { render } from 'mustache';
 import { renderSync } from 'sass';
-import { format as fmt } from 'util';
+import { author, description, version } from '../package.json';
 
 (() => {
-	const { css } = renderSync({
+	const compiled = renderSync({
 		indentType: 'tab',
 		indentWidth: 1,
 		outputStyle: 'expanded',
 		file: 'src/index.scss'
 	});
 
-	const final = css
+	let css = compiled.css
 		.toString('utf8')
 		.replace('@charset "UTF-8";', '')
 		.replace(/#3498db/gim, 'var(--color-generic-accent)');
 
 	const meta = readFileSync(`${__dirname}/res/metadata.txt`);
-	const content = fmt(meta.toString('utf8'), final);
+	css = render(meta.toString('utf8'), { author, description, version, css });
 
-	const rc = readFileSync(resolve(__dirname, '..', '.prettierrc'));
-	const options = JSON.parse(rc.toString('utf8'));
-
-	writeFileSync(
-		`${process.cwd()}/genius-dark.user.css`,
-		format(content, { parser: 'scss', ...options })
-	);
+	writeFileSync(`${process.cwd()}/genius-dark.user.css`, css);
 })();
